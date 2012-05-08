073420fe0803b55ff1b264da1f533d5b8d679445
Michał Pawelski <pawelski#interia.eu>
2012-05-08 11:52:44 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Europodatki::Application.config.secret_token = '2553c71051ea33fe9d9027e169805c9bc318cda51072d9bf7af6ec1941528bd141fdd358012ce9afd231f4da2a12cc53d1207b590ddb743cf91ea6be0e9f4555'
